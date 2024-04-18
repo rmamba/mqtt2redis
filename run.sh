24218cbba91a7322830f24fd3d900556b0dcc59f
rmamba <rmamba@gmail.com>
2024-04-18 16:25:16 +0700

    fix run script

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+# open vpn connection for redis server
 openvpn --config /openvpn/client.ovpn &
+
+# run the main app
 node server.js
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
